4b421600d19287aa47a841651af8a7a8e679e7de
sputnika <sputnika91@gmail.com>
2019-06-01 18:12:13 -0400

    done

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,11 +6,20 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
-    
+    private String address;
+    private String number;
+    private String eyes;
+    private String hair;
+    private String gender;
 
     public Person() {
         name = "";
         age= Integer.MAX_VALUE;
+        address="12345 Pink Valley Way";
+        number = "1234567890";
+        eyes = "yellow";
+        hair = "red";
+        gender = "female";
 
     }
 
@@ -27,6 +36,16 @@ public class Person {
         this.age=age;
     }
 
+    public Person(String name, int age, String address, String number, String eyes, String hair,String gender){
+        this.name=name;
+        this.age = age;
+        this .address = address;
+        this.number= number;
+        this.eyes=eyes;
+        this.hair=hair;
+        this.gender=gender;
+    }
+
     public void setName(String name) {
         this.name=name;
     }
@@ -35,11 +54,49 @@ public class Person {
        this.age=age;
     }
 
+    public void setAddress(String address) {
+        this.address=address;
+    }
+
+    public void setNumber(String number) {
+        this.number=number;
+    }
+
+    public void setEyes(String eyes) {
+        this.eyes=eyes;
+    }
+
+    public void setHair(String hair) {
+        this.hair=hair;
+    }
+    public void setGender(String gender) {
+        this.gender=gender;
+    }
+
     public String getName() {
         return this.name;
     }
 
     public Integer getAge() {
         return this.age;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public String getEyes() {
+        return this.eyes;
+    }
+
+    public String getHair() {
+        return this.hair;
+    }
+    public String getGender() {
+        return this.gender;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -69,6 +69,40 @@ public class TestPerson {
     }
 
     @Test
+    public void testConstructorWithNameAgeAddressNumberEyesHairGender() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Leon";
+        String expectedAddress = "12345 Pink Valley Way";
+        String expectedNumber = "1234567890";
+        String expectedEyes = "yellow";
+        String expectedHair = "red";
+        String expectedGender = "female";
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedAddress,expectedNumber,expectedEyes,expectedHair,expectedGender);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        String actualAddress = person.getAddress();
+        String actualNumber = person.getNumber();
+        String actualEyes = person.getEyes();
+        String actualHair = person.getHair();
+        String actualGender = person.getGender();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAddress,actualAddress);
+        Assert.assertEquals(expectedNumber,actualNumber);
+        Assert.assertEquals(expectedEyes,actualEyes);
+        Assert.assertEquals(expectedHair,actualHair);
+        Assert.assertEquals(expectedGender,actualGender);
+    }
+
+
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -93,6 +127,75 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetAddress() {
+        // Given
+        Person person = new Person();
+        String expected = "12345 Pink Valley Way";
+
+        // When
+        person.setAddress(expected);
+        String actual = person.getAddress();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "1234567890";
+
+        // When
+        person.setNumber(expected);
+        String actual = person.getNumber();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetEyes() {
+        // Given
+        Person person = new Person();
+        String expected = "yellow";
+
+        // When
+        person.setEyes(expected);
+        String actual = person.getEyes();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetHair() {
+        // Given
+        Person person = new Person();
+        String expected = "red";
+
+        // When
+        person.setHair(expected);
+        String actual = person.getHair();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetGender() {
+        // Given
+        Person person = new Person();
+        String expected = "Female";
+
+        // When
+        person.setGender(expected);
+        String actual = person.getGender();
+
+        // Then
         Assert.assertEquals(expected, actual);
     }
 }
