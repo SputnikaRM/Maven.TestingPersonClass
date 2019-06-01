b53cd70051e3066eccd1da721d51ddac8b2332f8
sputnika <sputnika91@gmail.com>
2019-06-01 14:53:57 -0400

    update

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -8,28 +8,37 @@ public class Person {
     private int age;
 
     public Person() {
+        name = "";
+        age= Integer.MAX_VALUE;
+
     }
 
     public Person(int age) {
+        age = this.getAge();
     }
 
     public Person(String name) {
+        name=this.getName();
     }
 
     public Person(String name, int age) {
+        name = getName();
+        age= getAge();
     }
 
     public void setName(String name) {
+        name = getName();
     }
 
     public void setAge(int age) {
+        age=getAge();
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
